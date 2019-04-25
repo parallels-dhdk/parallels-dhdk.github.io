68a92309339dce034a8e1fc3cf2bb0a669b2011a
Michela <michela.parma@hotmail.it>
2019-04-25 08:34:48 +0200

    hbhjb

--- move.js
+++ move.js
@@ -1,23 +1,24 @@
 <script>
-$( document ).ready(function() {
-var caller = $(".item1 a");
-var container = $(".grid-container");
+  
 
-function foo()
-{
-    var randX = Math.floor(Math.random() * (window.innerWidth - 100));
-    var randY = Math.floor(Math.random() * (window.innerHeight - 100));
-    console.log([randX, randY]);
-    caller.stop().animate({"left": randX + "px", "top": randY + "px"});
-}
+  var caller = $(".item1 a");
+  var container = $(".item1");
+  
+  function foo()
+  {
+      var randX = Math.floor(Math.random() * (container[0].offsetWidth - 150));
+      var randY = Math.floor(Math.random() * -(container[0].offsetHeight - 100));
+      console.log([randX, randY]);
+      caller.stop().animate({"left": randX + "px", "top": randY + "px"});
+  }
+  
+  $(document).ready(function() {
+      
+      caller.on('mouseenter', foo);
+      caller.on('click', function(){
+          alert('clicked!');
+      });
+      
+  })
 
-$(document).ready(function() {
-    
-    caller.on('mouseenter', foo);
-    caller.on('click', function(){
-        alert('clicked!');
-    });
-    
-})
-})
-</script>
+  </script>
