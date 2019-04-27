c2b9f8e391666f03028ee7047fa048c3aa6f5c1a
Michela <michela.parma@hotmail.it>
2019-04-27 13:56:29 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -86,3 +86,32 @@ $("#2040").click(function() {
     $("#css").load('2040.css')
 })    
 })
+
+
+
+
+
+
+
+
+
+
+var caller = $(".item1 a");
+  var container = $(".item1");
+  
+  function foo()
+  {
+      var randX = Math.floor(Math.random() * (container[0].offsetWidth - 150));
+      var randY = Math.floor(Math.random() * -(container[0].offsetHeight - 100));
+      console.log([randX, randY]);
+      caller.stop().animate({"left": randX + "px", "top": randY + "px"});
+  }
+  
+  $(document).ready(function() {
+      
+      caller.on('mouseenter', foo);
+      caller.on('click', function(){
+          alert('clicked!');
+      });
+      
+  })
