99944a18de3090bba424d87ff8633091341dd82a
Michela <michela.parma@hotmail.it>
2019-05-05 13:33:49 +0200

    dfsf

--- javascript.js
+++ javascript.js
@@ -1,93 +1,92 @@
  
 $(document).ready(function () 
-    { 
+{ 
+$("#css").load('1560.css')
+
+
+//about button in nav
+$(".about-button").click(function() {
+    $("#mainbody").html("") //returns the content (innerHTML) of the mainbody and empty it.
+    $("#mainbody").load('about.html') //loads data from a document and puts the returned data into mainbody.
+})
+
+//documentation button in nav
+$(".documentation-button").click(function() {
+    $("#mainbody").html("")
+    $("#mainbody").load('documentation.html')
+})
+
+//index button in navbar
+$(".index-button").click(function() {
+    $("#mainbody").html("")
+    $("#mainbody").load('index-text.html') 
+})
+
+//year buttons in timeline 
+$("#1560").click(function() {
+    $("#css").html("")
     $("#css").load('1560.css')
+})
+  
+$("#1860").click(function() {
+    $("#css").html("")
+    $("#css").load('1860.css')
+})
 
+$("#1927").click(function() {
+    $("#css").html("")
+    $("#css").load('1927.css')
+})     
 
-    //about button in nav
-    $(".about-button").click(function() {
-        $("#mainbody").html("") //returns the content (innerHTML) of the mainbody and empty it.
-        $("#mainbody").load('about.html') //loads data from a document and puts the returned data into mainbody.
-    })
-
-    //documentation button in nav
-    $(".documentation-button").click(function() {
-        $("#mainbody").html("")
-        $("#mainbody").load('documentation.html')
-    })
-
-    //index button in navbar
-    $(".index-button").click(function() {
-        $("#mainbody").html("")
-        $("#mainbody").load('index-text.html') 
-    })
-
-    //year buttons in timeline 
-    $("#1560").click(function() {
-        $("#css").html("")
-        $("#css").load('1560.css')
-    })
-      
-    $("#1860").click(function() {
-        $("#css").html("")
-        $("#css").load('1860.css')
-    })
-
-    $("#1927").click(function() {
-        $("#css").html("")
-        $("#css").load('1927.css')
-    })     
-
-    $("#1968").click(function() {
-        $("#css").html("")
-        $("#css").load('1968.css')
-    })  
-        
-    $("#1980").click(function() {
-        $("#css").html("")
-        $("#css").load('1980.css')
-    })
-        
-    $("#2040").click(function() {
-        $("#css").html("")
-        $("#css").load('2040.css')
-    })    
+$("#1968").click(function() {
+    $("#css").html("")
+    $("#css").load('1968.css')
+})  
     
+$("#1980").click(function() {
+    $("#css").html("")
+    $("#css").load('1980.css')
+})
+    
+$("#2040").click(function() {
+    $("#css").html("")
+    $("#css").load('2040.css')
+})    
+
 })
 
 
 //Articles 
 function art1(){
-    $("#mainbody").html("") //returns the content (innerHTML) of the mainbody and empty it.
-    $("#mainbody").load('japan.html') //loads data from a document and puts the returned data into mainbody.
+$("#mainbody").html("") //returns the content (innerHTML) of the mainbody and empty it.
+$("#mainbody").load('japan.html') //loads data from a document and puts the returned data into mainbody.
 }
 
 function art2(){
-    $("#mainbody").html("")
-    $("#mainbody").load('alabama.html')
+$("#mainbody").html("")
+$("#mainbody").load('alabama.html')
 }
 
 function art3(){
-    $("#mainbody").html("")
-    $("#mainbody").load('lottery.html')
+$("#mainbody").html("")
+$("#mainbody").load('lottery.html')
 }
 
 function art4(){
-    $("#mainbody").html("")
-    $("#mainbody").load('delvey.html')
+$("#mainbody").html("")
+$("#mainbody").load('delvey.html')
 }
 
 function art5(){
-    $("#mainbody").html("")
-    $("#mainbody").load('machine.html')
+$("#mainbody").html("")
+$("#mainbody").load('machine.html')
 }
 
 function art6(){
-    $("#mainbody").html("")
-    $("#mainbody").load('eudirective.html')
+$("#mainbody").html("")
+$("#mainbody").load('eudirective.html')
 }
 
 
 
-    
-  
+
