77294cbc54d45eb1eaf12d2dc2485411a16eb4b4
Michela <michela.parma@hotmail.it>
2019-05-05 13:41:03 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,92 +1,187 @@
- 
 $(document).ready(function () 
-{ 
-$("#css").load('1560.css')
+    { 
+    //about button in nav
+    $(".about-button").click(function() {
+        $("#mainbody").html("")
+        $("#mainbody").load('about.html')
+    })
+
+    //documentation button in nav
+    $(".documentation-button").click(function() {
+        $("#mainbody").html("")
+        $("#mainbody").load('documentation.html')
+    })
 
 
-//about button in nav
-$(".about-button").click(function() {
-    $("#mainbody").html("") //returns the content (innerHTML) of the mainbody and empty it.
-    $("#mainbody").load('about.html') //loads data from a document and puts the returned data into mainbody.
-})
-
-//documentation button in nav
-$(".documentation-button").click(function() {
-    $("#mainbody").html("")
-    $("#mainbody").load('documentation.html')
-})
-
-//index button in navbar
-$(".index-button").click(function() {
-    $("#mainbody").html("")
-    $("#mainbody").load('index-text.html') 
-})
-
-//year buttons in timeline 
-$("#1560").click(function() {
-    $("#css").html("")
+    //year buttons in timeline 
+    var quaranta=false 
     $("#css").load('1560.css')
+
+
+    $("#1560").click(function() {
+        quaranta=false
+        $("#css").html("")
+        $("#css").load('1560.css')
+    })
+      
+    $("#1860").click(function() {
+        quaranta=false
+        $("#css").html("")
+        $("#css").load('1860.css')
+    })
+
+    $("#1927").click(function() {
+        quaranta=false
+        $("#css").html("")
+        $("#css").load('1927.css')
+    })     
+
+    $("#1968").click(function() {
+        quaranta=false
+        $("#css").html("")
+        $("#css").load('1968.css')
+    })  
+        
+       
+    $("#1980").click(function() {
+        quaranta=false
+        $("#css").html("")
+        $("#css").load('1980.css')
+    })
+        
+    $("#2040").click(function() {
+        quaranta= true
+        $("#css").html("")
+        $("#css").load('2040.css')
+    })    
+    
+
+        //index button in navbar
+        $(".index-button").click(function() {
+        $("#mainbody").html("")
+        $("#mainbody").load('index-text.html', function(){ //callback function: what is invoked after the first function if that first function completes
+
+     
+            item1 = $(".item1 a");
+            container1 = $(".item1");
+
+            item2 = $(".item2 a");
+            container2 = $(".item2");
+
+            item3 = $(".item3 a");
+            container3 = $(".item3");
+
+            item4 = $(".item4 a");
+            container4 = $(".item4");
+
+            item5 = $(".item5 a");
+            container5 = $(".item5");
+
+            item6 = $(".item6 a");
+            container6 = $(".item6");
+
+            item1.on('mouseenter', function(){ 
+                if (quaranta) {
+                foo(item1, container1);
+                }
+            })
+
+            item2.on('mouseenter', function(){ 
+                if (quaranta) {
+                foo(item2, container2);
+                }
+            })
+
+            item3.on('mouseenter', function(){ 
+                if (quaranta) {
+                foo(item3, container3);
+                }
+            })
+
+            item4.on('mouseenter', function(){ 
+                if (quaranta) {
+                foo(item4, container4);
+                }
+            })
+
+            item5.on('mouseenter', function(){ 
+                if (quaranta) {
+                foo(item5, container5);
+                }
+            })
+
+            item6.on('mouseenter', function(){ 
+                if (quaranta) {
+                foo(item6, container6);
+                }
+            }) 
+        })
 })
+
+    
+
+    
+    var item1 = $(".item1 a");
+    var container1 = $(".item1");
+
+    var item2 = $(".item2 a");
+    var container2 = $(".item2");
+
+    var item3 = $(".item3 a");
+    var container3 = $(".item3");
+
+    var item4 = $(".item4 a");
+    var container4 = $(".item4");
+
+    var item5 = $(".item5 a");
+    var container5 = $(".item5");
+
+    var item6 = $(".item6 a");
+    var container6 = $(".item6");
+
+
+    
+  function foo(item, container) //Animated link in 2040 homepage
+  {
+      var randX = Math.floor(Math.random() * (container[0].offsetWidth - 150));
+      var randY = Math.floor(Math.random() * -(container[0].offsetHeight - 100));
+      item.stop().animate({"left": randX + "px", "top": randY + "px"});
+  }
   
-$("#1860").click(function() {
-    $("#css").html("")
-    $("#css").load('1860.css')
+      
+    item1.on('mouseenter', function(){ 
+          if (quaranta) {
+          foo(item1, container1);
+          }
+    })
+
+    item2.on('mouseenter', function(){ 
+        if (quaranta) {
+        foo(item2, container2);
+        }
+    })
+
+    item3.on('mouseenter', function(){ 
+        if (quaranta) {
+        foo(item3, container3);
+        }
+    })
+
+    item4.on('mouseenter', function(){ 
+        if (quaranta) {
+        foo(item4, container4);
+        }
+    })
+
+    item5.on('mouseenter', function(){ 
+        if (quaranta) {
+        foo(item5, container5);
+        }
+    })
+
+    item6.on('mouseenter', function(){ 
+        if (quaranta) {
+        foo(item6, container6);
+        }
+    })
 })
-
-$("#1927").click(function() {
-    $("#css").html("")
-    $("#css").load('1927.css')
-})     
-
-$("#1968").click(function() {
-    $("#css").html("")
-    $("#css").load('1968.css')
-})  
-    
-$("#1980").click(function() {
-    $("#css").html("")
-    $("#css").load('1980.css')
-})
-    
-$("#2040").click(function() {
-    $("#css").html("")
-    $("#css").load('2040.css')
-})    
-
-})
-
-
-//Articles 
-function art1(){
-$("#mainbody").html("") //returns the content (innerHTML) of the mainbody and empty it.
-$("#mainbody").load('japan.html') //loads data from a document and puts the returned data into mainbody.
-}
-
-function art2(){
-$("#mainbody").html("")
-$("#mainbody").load('alabama.html')
-}
-
-function art3(){
-$("#mainbody").html("")
-$("#mainbody").load('lottery.html')
-}
-
-function art4(){
-$("#mainbody").html("")
-$("#mainbody").load('delvey.html')
-}
-
-function art5(){
-$("#mainbody").html("")
-$("#mainbody").load('machine.html')
-}
-
-function art6(){
-$("#mainbody").html("")
-$("#mainbody").load('eudirective.html')
-}
-
-
-
-
